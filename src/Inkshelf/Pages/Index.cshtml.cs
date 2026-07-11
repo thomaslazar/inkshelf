2285a092962aac91a80fe6ff6f5278fbfe89d88f
Thomas Lazar <thomas@razal.de>
2026-07-11 13:29:28 +0000

    feat: libraries list page

--- src/Inkshelf/Pages/Index.cshtml.cs
+++ src/Inkshelf/Pages/Index.cshtml.cs
@@ -1,10 +1,16 @@
+using Inkshelf.Abs;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Inkshelf.Pages;
 
 public class IndexModel : PageModel
 {
-    public void OnGet()
-    {
-    }
+    private readonly AbsSession _session;
+    private readonly AbsClient _client;
+    public IndexModel(AbsSession session, AbsClient client) { _session = session; _client = client; }
+
+    public List<AbsLibrary> Libraries { get; private set; } = new();
+
+    public async Task OnGetAsync(CancellationToken ct) =>
+        Libraries = await _session.ExecuteAsync((tok, c) => _client.GetLibrariesAsync(tok, c), ct);
 }
