6357aed628dddef0ef7e1682c6c32b5670b466bf
Thomas Lazar <thomas@razal.de>
2026-07-13 13:02:10 +0000

    style: format SortLinksTests per dotnet format

--- tests/Inkshelf.Tests/SortLinksTests.cs
+++ tests/Inkshelf.Tests/SortLinksTests.cs
@@ -4,13 +4,16 @@ namespace Inkshelf.Tests;
 
 public class SortLinksTests
 {
-    [Fact] public void Inactive_field_goes_ascending()
+    [Fact]
+    public void Inactive_field_goes_ascending()
     { var (s, d) = SortLinks.Next("addedAt", "media.metadata.title", false); Assert.Equal("addedAt", s); Assert.False(d); }
 
-    [Fact] public void Ascending_active_goes_descending()
+    [Fact]
+    public void Ascending_active_goes_descending()
     { var (s, d) = SortLinks.Next("t", "t", false); Assert.Equal("t", s); Assert.True(d); }
 
-    [Fact] public void Descending_active_turns_off()
+    [Fact]
+    public void Descending_active_turns_off()
     { var (s, d) = SortLinks.Next("t", "t", true); Assert.Null(s); Assert.False(d); }
 
     [Theory]
